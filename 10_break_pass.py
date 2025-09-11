ab67d6023329cf21286f6918b7e92ff1e68e8e34
yeonjae <yeonjae@email.com>
2025-09-11 11:20:57 +0900

    continue 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,35 @@
 cup = 0
 running = True
 
-while running:
-    cup += 1  # += 더해서 대입 (cup + 1)
-    print(cup)
-    if cup == 10:
-        # running = False
-        break  # cup이 10이 되면 종료
+# while running:
+#     cup += 1  # += 더해서 대입 (cup + 1)
+#     print(cup)
+#     if cup == 10:
+#         # running = False
+#         break  # cup이 10이 되면 종료
+#
+# print('while 문 종료')
 
-print('while 문 종료')
+for i in range(1,10):
+
+#3,6,9를 제외한 숫자만 실행
+
+    # if i ==3:
+    #     continue
+    # if i ==6:
+    #     continue
+    # if i ==9:
+    #     continue
+
+    # or 조건
+    if i ==3 | i ==6 | i==9:
+        continue
+
+    # 나머지
+    if i % 3 == 0:
+        continue
+
+    if i in [3,6,9]: #i 안에 3,6,9가 포함되어 있다면?
+        continue
+
+    print(i)
