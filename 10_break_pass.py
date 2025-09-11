875f50945b2c907e07bf091b3b9eda4d5e796c72
yeonjae <yeonjae@email.com>
2025-09-11 11:01:41 +0900

    break 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,11 @@
 cup = 0
 running = True
 
-while running :
-    cup += 1 # += 더해서 대입 (cup + 1)
+while running:
+    cup += 1  # += 더해서 대입 (cup + 1)
     print(cup)
     if cup == 10:
-        #running = False
-        break #cup이 10이 되면 종료
-        
+        # running = False
+        break  # cup이 10이 되면 종료
+
 print('while 문 종료')
