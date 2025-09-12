4fac7e5a5fb9016779daa9f6173370f5e3d5c825
yeonjae <yeonjae@email.com>
2025-09-12 12:48:40 +0900

    dic 함수 문제 1,2

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -28,3 +28,36 @@ print(dic.items())
 items = list(dic.items()) # list 안에 tuple이 들어가 있는 형태
 print(items)
 
+# 값을 가져오기
+print(dic.get('name')) # get 메서드 활용
+print(dic['phone'])
+
+# dic 안에 있는 모든 키와 값을 키:값 형태로 출력해 보자
+# 1. 키를 뽑아낸 다음, 키를 가지고 값을 뽑아내는 방법
+for keys in dic.keys():
+    print(f'{keys}: {dic[keys]}')
+# 2. 키와 값을 동시에 뽑아낸 다음 거기서 키와 값을 각각 추출하는 방식
+for items in dic.items():
+    # print(items) # ('name', 'hong,gil-dong')으로 출력 tuple형태 
+    print(f'{items[0]}: {items[1]}') #즉, tuple이 불러오는 방식 : print(tu1[1]) 사용
+    
+#내가 한 방법
+# keys = list(dic.keys())
+# print(f'{keys[0]}:{dic['name']}, {keys[1]}:{dic["phone"]}, {keys[2]}:{dic["friends"]}')
+
+members = {
+    'kim':63, 'lee':88, 'park':97, "gang":77, "hwang":100, "ga":65,
+    "na":92, "la":90, "wang":100, "gu":79
+}
+
+# 90이상인 사람의 이름만 출력
+for item in members.items(): # members의 키와 값을 둘다 가져온다 # kim:63 (0번방 : 1번방)
+    if item[1]>=90: # 1번방이 점수
+        print(f'이름 : {item[0]}') #이름만 출력하는 것이기 때문에 0번방만
+        #print(f'이름 : {item[0]} 점수 : {item[1]}') # 둘다 뽑아낸다면?
+
+# 내가 한 방법
+# 만약 members에 있는 사람의 점수가 90 점 이상이라면? 이름만 출력
+# for keys in members.keys():
+#     if members[keys] >= 90:
+#         print(keys)
