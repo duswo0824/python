00965c25c13b9b4c4fa4024470a1e1a428181065
yeonjae <yeonjae@email.com>
2025-09-15 16:23:05 +0900

    모듈 수수정

--- advence/01_module_use.py
+++ advence/01_module_use.py
@@ -1,10 +1,10 @@
 # from 모듈 import 함수
-# 사용할 함수를 미리 불러놓고 사용하는 방법
+# 사용할 함수를 미리 불러놓고 사용하는 방법 #필요한 것만
 from oper import sum # oper의 파일에서(from) sum이라는 함수를 불러오겠다.(import)
 print(f'sum() 함수 실행 : {sum(5,10)}')
 
 # import 모듈
-# 일단 모듈을 불러놓고 모듈로부터 원하는 함수를 사용하는 방법 #필요한 것만
+# 일단 모듈을 불러놓고 모듈로부터 원하는 함수를 사용하는 방법
 import oper as op #as를 사용해서 이름줄임 (별칭)
 print(f'minus() 함수 실행 : {op.minus(10,5)}') #op .(하위의) minus 함수
 
