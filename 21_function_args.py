3e049c2c34284625d7a7b750b0260d53d957fbbb
yeonjae <yeonjae@email.com>
2025-09-15 09:48:52 +0900

    함수 인자값

---
+++ 21_function_args.py
@@ -0,0 +1,6 @@
+def plus(num): #puls를 실행하면 
+    result = num + 5 # num이라는 인자값에 +5를하고
+    return result # 반환하여 보여준다
+
+print(plus(5)) #10
+print(plus()) # plus() missing 1 required positional argument: 'num'
