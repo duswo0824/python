459d97dc2e8eb49f422377fdbf501759191235ae
yeonjae <yeonjae@email.com>
2025-09-15 10:01:57 +0900

    함수 인자값(튜플로 받기)

--- 21_function_args.py
+++ 21_function_args.py
@@ -6,3 +6,13 @@ def plus(num=0): #puls를 실행하면
 print(plus(5)) # 10
 # 아무 것도 넣지 않았을 때 대체할 수 있는 값 = 기본값 (num=0)을 넣으면 # 5
 print(plus()) # plus() missing 1 required positional argument: 'num'
+
+# 인자값의 종류를 튜플(수정이 불가능한 List 형태)로만 받겠다.
+def tuple_args(*numbers):
+    print(numbers) # 튜플 형태로 반환
+    total = 0
+    for num in numbers:
+        total += num
+    return total # return은 무조건 함수 하나에 한번
+
+print(tuple_args(1, 2, 3, 4, 5))
