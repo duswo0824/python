0ba7a28c48488dd06921b92455ffdca60fa8e8f8
yeonjae <yeonjae@email.com>
2025-09-16 15:52:46 +0900

    다중 예외 처리 방식

--- advence/12_multi.py
+++ advence/12_multi.py
@@ -1,20 +1,18 @@
 # try 코드 안에서 여러개의 에러가 발생한 경우
+import traceback
 
 # 1. 생겨난 예외마다 각기 다른 처리를 해주고 싶을 때
 try:
     pass
-except IndexError:
+except IOError: # 각 예외(에러 발생부분)마다 다른 처리 하나씩
     pass
 except KeyboardInterrupt:
     pass
 except ValueError:
     pass
 
-# 각 예외(에러 발생부분)마다 다른 처리 하나씩
-
-
 # 2. 어떤 예외가 발생하던지 동일한 처리를 하고 싶을 때
 try:
     pass
-except Exception:
-    pass
+except Exception as e: # 예외의 최상위 부모이기 때문에 다른 자식 예외들이 모두 들어올 수 있음
+    traceback.print_exc()
