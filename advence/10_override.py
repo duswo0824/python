cab570ddc8456b3c623a83141b506ec6a148e730
yeonjae <yeonjae@email.com>
2025-09-16 14:50:54 +0900

    오버라이드 구현

---
+++ advence/10_override.py
@@ -0,0 +1,19 @@
+class Car:
+    def start(self):
+        print('시동이 걸린다.')
+
+    def run(self):
+        print('차가 시속 50km로 달린다.')
+
+    def stop(self):
+        print('차가 멈춘다.')
+
+class MyCar(Car):
+
+    def run(self): # 부모와 같은 메서드(함수)를 사용하면 override로 인식
+        print('차가 시속 200km로 달린다.')
+
+mc = MyCar()
+mc.start()
+mc.run()
+mc.stop()
